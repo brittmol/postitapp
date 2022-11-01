347ab12fd415125ca6f3462b3d23f793f0d8ea9b
Brittany Moliver <britt.moliver@gmail.com>
2022-11-01 14:38:24 -0400

    routes api index

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-// GET /api/set-token-cookie
-const asyncHandler = require("express-async-handler");
-const { setTokenCookie } = require("../../utils/auth.js");
-const { User } = require("../../db/models");
+// // GET /api/set-token-cookie
+// const asyncHandler = require("express-async-handler");
+// const { setTokenCookie } = require("../../utils/auth.js");
+// const { User } = require("../../db/models");
 // router.get(
 //   "/set-token-cookie",
 //   asyncHandler(async (req, res) => {
